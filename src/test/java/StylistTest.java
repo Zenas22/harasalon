c07ca8ef1c13577ed0947c9733c680e16f8c4ba8
Zenas Muli <zenas2294@gmail.com>
2019-05-26 22:59:03 +0300

    Added test to check validity of stylist constructor input

--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -8,4 +8,19 @@ public class StylistTest {
         Stylist testStylist = new Stylist(1,"Stacy","Nthenya","stacy@gmail.com",22);
         assertTrue(testStylist instanceof Stylist);
     }
+
+    @Test
+    public void newStylist_firstJina_secondJina_baruaPepe_isString() {
+        Stylist testStylist = new Stylist(1,"Stacy","Nthenya","stacy@gmail.com",22);
+        assertEquals("Stacy", testStylist.getFirstJina());
+        assertEquals("Nthenya", testStylist.getSecondJina());
+        assertEquals("stacy@gmail.com", testStylist.getBaruaPepe());
+    }
+
+    @Test
+    public void newStylist_umri_id_isInt() {
+        Stylist testStylist = new Stylist(1,"Stacy","Nthenya","stacy@gmail.com",22);
+        assertEquals(1, testStylist.getId());
+        assertEquals(22, testStylist.getUmri());
+    }
 }
