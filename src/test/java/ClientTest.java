55634786ca8fd4501cde2fa12c5d81c1864277a7
Zenas Muli <zenas2294@gmail.com>
2019-05-27 09:30:11 +0300

    Added clients test file basic functions

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,6 +1,22 @@
 import org.junit.*;
 import org.junit.Assert.*;
+import org.sql2o.Sql2o;
+import org.sql2o.Connection;
 
-public class ClientTest {
+public class ClientTest{
 
+    @Before
+    public void setUp(){
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", "zeus", "null");
+    }
+
+    @After
+    public void tearDown(){
+        try(Connection con = DB.sql2o.open()){
+            String deleteClientsql = "DELETE FROM clients *;";
+            String deleteStylistsql = "DELETE FROM stylists *;";
+            con.createQuery(deleteClientsql).executeUpdate();
+            con.createQuery(deleteStylistsql).executeUpdate();
+        }
+    }
 }
