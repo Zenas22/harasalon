c70d9e2d8fb1a479c3a45a38dcc762fdbdea4ad4
Zenas Muli <zenas2294@gmail.com>
2019-05-27 10:28:09 +0300

    Created a method that overrides the equals method if the similar clients are added

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -25,6 +25,19 @@ public class Client {
         return id;
     }
 
+    @Override
+    public boolean equals(Object otherClient){
+        if (!(otherClient instanceof Client)){
+            return false;
+        } else {
+            Client newClient = (Client) otherClient;
+            return this.getName().equals(newClient.getName()) &&
+                    this.getAge() == newClient.getAge() &&
+                    this.getHuduma().equals(newClient.getHuduma()) &&
+                    this.getEmail().equals(newClient.getEmail());
+        }
+    }
+
     public Client(String name, int age, String huduma, String email) {
         this.name = name;
         this.age = age;
