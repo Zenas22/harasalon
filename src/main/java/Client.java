e25f064235ef88b8fab554c6cb04c6e38c6c4f64
Zenas Muli <zenas2294@gmail.com>
2019-05-27 12:58:53 +0300

    Added database rule class and rule to client.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,9 +1,17 @@
+import org.sql2o.*;
+import java.util.List;
+
 public class Client {
     private String name;
     private int age;
     private String huduma;
     private String email;
     private int id;
+    private int stylistid;
+
+    public int getStylistid() {
+        return stylistid;
+    }
 
     public String getName() {
         return name;
@@ -34,14 +42,36 @@ public class Client {
             return this.getName().equals(newClient.getName()) &&
                     this.getAge() == newClient.getAge() &&
                     this.getHuduma().equals(newClient.getHuduma()) &&
-                    this.getEmail().equals(newClient.getEmail());
+                    this.getEmail().equals(newClient.getEmail()) &&
+                    this.getStylistid() == newClient.getStylistid();
         }
     }
 
-    public Client(String name, int age, String huduma, String email) {
+    public Client(String name, int age, String huduma, String email, int stylistid) {
         this.name = name;
         this.age = age;
         this.huduma = huduma;
         this.email = email;
+        this.stylistid = stylistid;
+    }
+
+    public static List<Client> all(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "SELECT id,name,age,huduma,email,stylistid FROM clients;";
+            return con.createQuery(sql).executeAndFetch(Client.class);
+        }
+    }
+
+    public void save(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO clients (name,age,huduma,email) VALUES (:name,:age,:huduma,:email);";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("age", this.age)
+                    .addParameter("huduma", this.huduma)
+                    .addParameter("email", this.email)
+                    .executeUpdate()
+                    .getKey();
+        }
     }
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -5,6 +5,9 @@ import org.sql2o.Connection;
 
 public class ClientTest{
 
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
     @Before
     public void setUp(){
         DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", "zeus", "null");
@@ -22,31 +25,48 @@ public class ClientTest{
 
     @Test
     public void client_instantiates_true(){
-        Client testClient = new Client("Stacy", 22, "32306402", "stacy@gmail.com");
+        Client testClient = new Client("Stacy", 22, "32306402", "stacy@gmail.com",1);
         assertTrue(testClient instanceof Client);
     }
 
     @Test
     public void client_instantiatesWithName_String(){
-        Client testClient = new Client("Stacy", 22, "32306402", "stacy@gmail.com");
+        Client testClient = new Client("Stacy", 22, "32306402", "stacy@gmail.com",1);
         assertEquals("Stacy", testClient.getName());
     }
 
     @Test
     public void client_instantiatesWithAge_Int(){
-        Client testClient = new Client("Stacy", 22, "32306402", "stacy@gmail.com");
+        Client testClient = new Client("Stacy", 22, "32306402", "stacy@gmail.com",1);
         assertEquals(22, testClient.getAge());
     }
 
     @Test
     public void client_instantiatesWithHuduma_String(){
-        Client testClient = new Client("Stacy", 22, "32306402", "stacy@gmail.com");
+        Client testClient = new Client("Stacy", 22, "32306402", "stacy@gmail.com",1);
         assertEquals("32306402", testClient.getHuduma());
     }
 
     @Test
     public void client_instantiatesWithEmail_String(){
-        Client testClient = new Client("Stacy", 22, "32306402", "stacy@gmail.com");
+        Client testClient = new Client("Stacy", 22, "32306402", "stacy@gmail.com",1);
         assertEquals("stacy@gmail.com", testClient.getEmail());
+    }
+
+    @Test
+    public void all_returnsAllClients_true(){
+        Client oneClient = new Client("Stacy", 22, "32306402", "stacy@gmail.com",1);
+        oneClient.save();
+        Client twoClient = new Client("Glory",18,"0008989","glory@gmail.com",1);
+        twoClient.save();
+        assertTrue(Client.all().contains(oneClient));
+        assertTrue(Client.all().contains(twoClient));
+    }
+
+    @Test
+    public void save_true(){
+        Client testClient = new Client("Stacy", 22, "32306402", "stacy@gmail.com",1);
+        testClient.save();
+        assertTrue(Client.all().get(1).equals(testClient));
     }
 }
