d8bd20caad345463696a801dc901937b415182b1
Zenas Muli <zenas2294@gmail.com>
2019-05-28 10:28:01 +0300

    Added layout.vtl and index.vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,24 @@
-import static spark.Spark.staticFileLocation;
+import java.util.Map;
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
 
 public class App{
     public static void main(String[] args){
         staticFileLocation("/public");
         String layout = "temlates/layout.vtl";
         String index = "templates/index.vtl";
+
+        get("/",(request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", index);
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
+
     }
 }
