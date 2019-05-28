ac7a905ace92d355117230865e864bd61c265c66
Zenas Muli <zenas2294@gmail.com>
2019-05-28 06:41:42 +0300

    Added test to confirm that new Stylists are instances of Stylists

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -3,6 +3,44 @@ import org.sql2o.Connection;
 
 import java.util.List;
 
-public class Stylist {
+public class Stylist{
+    private String firstname;
+    private String lastname;
+    private int age;
+    private int phone;
+    private String huduma;
+    private String email;
 
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public String getHuduma() {
+        return huduma;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Stylist(String firstname, String lastname, int age, int phone, String huduma, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.phone = phone;
+        this.huduma = huduma;
+        this.email = email;
+    }
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -4,15 +4,24 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.Arrays;
 
-public class StylistTest {
+public class StylistTest{
 
     @Before
-    public void setUp() {
+    public void setUp(){
         DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test","zeus","null");
     }
 
+    @Test
+    public void stylist_instantiates(){
+        Stylist testStylist = new Stylist("Zeus","Dagod",25,704832108,"323206402","zeus@gmail.com");
+        assertTrue(testStylist instanceof Stylist);
+    }
+
+    @Test
+    public
+
     @After
-    public void teardown() {
+    public void teardown(){
         try(Connection con = DB.sql2o.open()) {
             String deleteClientsQuery = "DELETE FROM clients *;";
             String deleteStylistsQuery = "DELETE FROM stylists *;";
