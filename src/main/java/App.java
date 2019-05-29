27ff9207ea06dbf670b4bbb0adb72d9a03bece07
Zenas Muli <zenas2294@gmail.com>
2019-05-29 09:03:38 +0300

    Added a route to stylists and the stylists.vtl file, created a form.vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ import java.util.Map;
 import java.util.HashMap;
 
 import spark.ModelAndView;
-//import spark.template.velocity.VelocityTemplateEngine;
+import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 
@@ -18,7 +18,20 @@ public class App{
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+        get("/stylists",(request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("stylists", Stylist.all());
+            model.put("template", "templates/stylists.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
 
-
+        get("/stylists/:id",(request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+            model.put("stylist", stylist);
+            model.put("clients", Client.all());
+            model.put("template", "templates/stylist.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
     }
 }
