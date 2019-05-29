4de477ea4ae4975faec695a57bf79560dd27fc84
Zenas Muli <zenas2294@gmail.com>
2019-05-29 17:19:37 +0300

    Prepared for maven deployment

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,8 @@
 import org.sql2o.*;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "zeus", "null");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "zeus", "null");
+
+    String connectionString = "jdbc:postgresql://ec2-50-19-254-63.compute-1.amazonaws.com:5432/d1qpct8hjmhbeh";
+    Sql2o sql2o = new Sql2o(connectionString, "ezfueykevmjzql","28045ca9513d24d24195cc73bcdfd73b1411cb4e5688727c260fa10104a981d9");
 }
